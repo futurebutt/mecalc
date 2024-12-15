5eaa102eca5695b403f486fc9bccecc4fe998914
futurebutt <foretoldposterior@gmail.com>
2024-12-15 13:37:43 -0500

    implemented TalentTree widget/container

--- src/test.py
+++ src/test.py
@@ -1,22 +1,9 @@
-import bisect
-import itertools
 from pathlib import Path
 
 from PyQt5 import uic
-from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QWidget
 
-import talents as tl
 import summarize as sm
-from widgets import TalentBar
-
-
-# 1-5: 3 points per level
-# 6-35: 2 points per level
-# 36-60: 1 point per level
-point_totals: list[int] = list(itertools.accumulate([3]*5 + [2]*30 + [1]*25))
-lvl_to_pts: dict[int, int] = dict(enumerate(point_totals, start=1))
-pts_to_lvl: dict[int, int] = {pts: lvl for lvl, pts in lvl_to_pts.items()}
 
 
 class MainWidget(QWidget):
@@ -26,69 +13,13 @@ class MainWidget(QWidget):
         super().__init__(parent)
         uic.loadUi(Path(__file__).with_name("test.ui"), self)
 
-        # ideally use more flexible container for creating/loading talent sets
-        self.talent_bars: list[TalentBar] = [self.TalentBar, self.TalentBar_2, self.TalentBar_3]
-        self.TalentBar.set_talent(tl.TalentAssaultTraining(0))
-        self.TalentBar_2.set_talent(tl.TalentFitness(0))
-        self.TalentBar_3.set_talent(tl.TalentPistols(0))
-
-        for tb in self.talent_bars:
-            tb.rankChanged.connect(self.talent_bars_rankChanged)
-
-        self.levelSpin.valueChanged.connect(self.levelSpin_valueChanged)
+        self.talentTree.set_class_soldier()
+        
         self.summaryButton.clicked.connect(self.summarizeButton_clicked)
 
-    @property
-    def total_points(self) -> int:
-        return lvl_to_pts[self.levelSpin.value()]
-
-    @property
-    def allocated_points(self) -> int:
-        return sum(tb.rank for tb in self.talent_bars)
-
-    @property
-    def unallocated_points(self) -> int:
-        return self.total_points - self.allocated_points
-
-    def update_total_point_display(self):
-        self.totalPointLabel.setText(str(self.total_points))
-
-    def update_unallocated_point_display(self):
-        self.unallocatedPointLabel.setText(str(self.unallocated_points))
-
-    def update_TalentBar_max_ranks(self):
-        for tb in self.talent_bars:
-            # Lowest of: 12, level + 1, talent rank + remaining points
-            rank = min((12, self.levelSpin.value() + 1, tb.rank + self.unallocated_points))
-            tb.set_max_rank(rank)
-
-    @pyqtSlot(int)
-    def levelSpin_valueChanged(self, _):
-        self.update_TalentBar_max_ranks()
-        self.update_unallocated_point_display()
-        self.update_total_point_display()
-
-    @pyqtSlot()
-    def talent_bars_rankChanged(self):
-        self.update_levelSpin_min()
-        self.update_TalentBar_max_ranks()
-        self.update_unallocated_point_display()
-        self.update_total_point_display()
-
-    def update_levelSpin_min(self):
-        # 1) Make sure there's at least as many total points as allocated
-        zero_indexed_level = bisect.bisect_left(point_totals, self.allocated_points)
-        min_lvl_by_total = zero_indexed_level + 1
-        # 2) Make sure ranks are not greater than level + 1
-        highest_rank = max([tb.rank for tb in self.talent_bars])
-        min_lvl_by_rank = highest_rank - 1
-        # Use high
-        min_level = max((min_lvl_by_total, min_lvl_by_rank))
-        self.levelSpin.setMinimum(min_level)
-    
     def summarizeButton_clicked(self):
         self.summaryTextEdit.clear()
-        talents = [tb.talent for tb in self.talent_bars]
+        talents = self.talentTree.get_talents()
         for summarize in (
             sm.summarize_Shepard,
             sm.summarize_Adrenaline_Burst,
--- src/widgets.py
+++ src/widgets.py
@@ -1,8 +1,21 @@
+import bisect
+import itertools
+
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, QSize
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QHBoxLayout, QLabel, QPushButton, QSizePolicy, QWidget
+from PyQt5.QtWidgets import (
+    QComboBox,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QSizePolicy,
+    QSpacerItem,
+    QSpinBox,
+    QVBoxLayout,
+    QWidget,
+)
 
-from talents import Talent
+import talents as tl
 
 
 class TalentPoint(QLabel):
@@ -15,7 +28,7 @@ class TalentPoint(QLabel):
         sizePolicy1.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
         self.setSizePolicy(sizePolicy1)
         self.setMinimumSize(QSize(19, 25))
-        self.setStyleSheet(u"border: 1px solid black;")
+        self.setStyleSheet("border: 1px solid black;")
 
     def set_on(self):
         self.setStyleSheet("background-color: rgb(0, 255, 0); border: 1px solid black;")
@@ -31,7 +44,7 @@ class TalentBar(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi()
-        self.talent = Talent(0)
+        self.talent = tl.Talent(0)
         self._max_rank: int = 2
         self._update_buttons()
 
@@ -131,11 +144,210 @@ class TalentBar(QWidget):
         self._max_rank = rank
         self._update_buttons()
     
-    def set_talent(self, talent: Talent):
+    def set_talent(self, talent: tl.Talent):
         assert talent.rank == 0
         self.talent = talent
         self.nameLabel.setText(talent.name)
 
+
+# 1-5: 3 points per level
+# 6-35: 2 points per level
+# 36-60: 1 point per level
+point_totals: list[int] = list(itertools.accumulate([3]*5 + [2]*30 + [1]*25))
+lvl_to_pts: dict[int, int] = dict(enumerate(point_totals, start=1))
+pts_to_lvl: dict[int, int] = {pts: lvl for lvl, pts in lvl_to_pts.items()}
+
+
+class TalentTree(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi()
+
+    def setupUi(self):
+        if not self.objectName():
+            self.setObjectName("Tree")
+        self.resize(431, 147)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        self.setMinimumSize(QSize(431, 0))
+        self.verticalLayout = QVBoxLayout(self)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.classStaticLabel = QLabel(self)
+        self.classStaticLabel.setObjectName("classStaticLabel")
+
+        self.horizontalLayout_2.addWidget(self.classStaticLabel)
+
+        self.classCombo = QComboBox(self)
+        self.classCombo.addItem("")
+        self.classCombo.addItem("")
+        self.classCombo.addItem("")
+        self.classCombo.addItem("")
+        self.classCombo.addItem("")
+        self.classCombo.addItem("")
+        self.classCombo.setObjectName("classCombo")
+
+        self.horizontalLayout_2.addWidget(self.classCombo)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.levelStaticLabel = QLabel(self)
+        self.levelStaticLabel.setObjectName("levelStaticLabel")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.levelStaticLabel.sizePolicy().hasHeightForWidth())
+        self.levelStaticLabel.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout.addWidget(self.levelStaticLabel)
+
+        self.levelSpin = QSpinBox(self)
+        self.levelSpin.setObjectName("levelSpin")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.levelSpin.sizePolicy().hasHeightForWidth())
+        self.levelSpin.setSizePolicy(sizePolicy2)
+        self.levelSpin.setReadOnly(False)
+        self.levelSpin.setMinimum(1)
+        self.levelSpin.setMaximum(60)
+
+        self.horizontalLayout.addWidget(self.levelSpin)
+
+        self.tpStaticLabel = QLabel(self)
+        self.tpStaticLabel.setObjectName("tpStaticLabel")
+        sizePolicy1.setHeightForWidth(self.tpStaticLabel.sizePolicy().hasHeightForWidth())
+        self.tpStaticLabel.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout.addWidget(self.tpStaticLabel)
+
+        self.totalPointLabel = QLabel(self)
+        self.totalPointLabel.setObjectName("totalPointLabel")
+        sizePolicy1.setHeightForWidth(self.totalPointLabel.sizePolicy().hasHeightForWidth())
+        self.totalPointLabel.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout.addWidget(self.totalPointLabel)
+
+        self.unallocStaticLabel = QLabel(self)
+        self.unallocStaticLabel.setObjectName("unallocStaticLabel")
+        sizePolicy1.setHeightForWidth(self.unallocStaticLabel.sizePolicy().hasHeightForWidth())
+        self.unallocStaticLabel.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout.addWidget(self.unallocStaticLabel)
+
+        self.unallocatedPointLabel = QLabel(self)
+        self.unallocatedPointLabel.setObjectName("unallocatedPointLabel")
+        sizePolicy1.setHeightForWidth(self.unallocatedPointLabel.sizePolicy().hasHeightForWidth())
+        self.unallocatedPointLabel.setSizePolicy(sizePolicy1)
+
+        self.horizontalLayout.addWidget(self.unallocatedPointLabel)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.classStaticLabel.setText("Class:")
+        self.classCombo.setItemText(0, "Soldier")
+        self.classCombo.setItemText(1, "Engineer")
+        self.classCombo.setItemText(2, "Adept")
+        self.classCombo.setItemText(3, "Infiltrator")
+        self.classCombo.setItemText(4, "Vanguard")
+        self.classCombo.setItemText(5, "Sentinel")
+
+        self.levelStaticLabel.setText("Level:")
+        self.tpStaticLabel.setText("Total Points:")
+        self.totalPointLabel.setText("3")
+        self.unallocStaticLabel.setText("Unallocated Points:")
+        self.unallocatedPointLabel.setText("3")
+
+        self.levelSpin.valueChanged.connect(self.levelSpin_valueChanged)        
+    
+    def add_talent_bar(self, talent: tl.Talent):
+        num_bars = len(self.findChildren(TalentBar))
+        bar = TalentBar(self)
+        bar.setObjectName(f"talentbar_{num_bars + 1}")
+        bar.set_talent(talent)
+        bar.rankChanged.connect(self.talent_bars_rankChanged)
+        self.verticalLayout.insertWidget(num_bars + 1, bar)
+    
+    def delete_all_talent_bars(self):
+        for bar in self.findChildren(TalentBar):
+            self.verticalLayout.removeWidget(bar)
+            bar.deleteLater()
+        self.levelSpin.setValue(1)
+
+    @property
+    def total_points(self) -> int:
+        return lvl_to_pts[self.levelSpin.value()]
+
+    @property
+    def allocated_points(self) -> int:
+        return sum(bar.rank for bar in self.findChildren(TalentBar))
+
+    @property
+    def unallocated_points(self) -> int:
+        return self.total_points - self.allocated_points
+
+    def update_total_point_display(self):
+        self.totalPointLabel.setText(str(self.total_points))
+
+    def update_unallocated_point_display(self):
+        self.unallocatedPointLabel.setText(str(self.unallocated_points))
+
+    def update_TalentBar_max_ranks(self):
+        for bar in self.findChildren(TalentBar):
+            # Lowest of: 12, level + 1, talent rank + remaining points
+            rank = min((12, self.levelSpin.value() + 1, bar.rank + self.unallocated_points))
+            bar.set_max_rank(rank)
+
+    @pyqtSlot(int)
+    def levelSpin_valueChanged(self, _):
+        self.update_TalentBar_max_ranks()
+        self.update_unallocated_point_display()
+        self.update_total_point_display()
+
+    @pyqtSlot()
+    def talent_bars_rankChanged(self):
+        self.update_levelSpin_min()
+        self.update_TalentBar_max_ranks()
+        self.update_unallocated_point_display()
+        self.update_total_point_display()
+
+    def update_levelSpin_min(self):
+        # 1) Make sure there's at least as many total points as allocated
+        zero_indexed_level = bisect.bisect_left(point_totals, self.allocated_points)
+        min_lvl_by_total = zero_indexed_level + 1
+        # 2) Make sure ranks are not greater than level + 1
+        highest_rank = max([bar.rank for bar in self.findChildren(TalentBar)])
+        min_lvl_by_rank = highest_rank - 1
+        # Use high
+        min_level = max((min_lvl_by_total, min_lvl_by_rank))
+        self.levelSpin.setMinimum(min_level)
+
+    def set_class_soldier(self):
+        self.delete_all_talent_bars()
+        self.add_talent_bar(tl.TalentPistols(0))
+        self.add_talent_bar(tl.TalentAssaultTraining(0))
+        self.add_talent_bar(tl.TalentFitness(0))
+    
+    def get_talents(self):
+        return [bar.talent for bar in self.findChildren(TalentBar)]
+    
 
 if __name__ == "__main__":
     import sys
