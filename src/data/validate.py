9e328fcbd4ec2be33e61f5cd105871e3c672e708
futurebutt <foretoldposterior@gmail.com>
2025-01-03 16:08:04 -0500

    more validation refactoring

--- src/data/validate.py
+++ src/data/validate.py
@@ -1,171 +1,106 @@
 import json
 import string
+from collections.abc import Callable
+from enum import Enum
 from typing import Any
 
-from venums import *
+from venums import AbsoluteBonus, PercentBonus, Specialization, TalentDataType
 
 
+MIN_NAME_LENGTH = 1
+MAX_NAME_LENGTH = 16
 TALENT_RANKS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-TALENT_DATA_TYPES = [
-    NAME := "name",
-    ROOT := "root",
-    PERCENT_BONUS := "percent_bonus",
-    ABSOLUTE_BONUS := "absolute_bonus",
-    SPECIALIZATION := "specialization"
-]
-MIN_NAME_LEN = 1
-MAX_NAME_LEN = 16
 
 
-def is_talent_file_valid(filename: str) -> bool:
-
+def validate_talent_file(filename: str) -> bool:
     with open(filename, "r") as f:
         talent_data: dict = json.load(f)
-
-    valid = is_talent_data_valid(talent_data)
-    return valid
+    return validate_talent_data(talent_data)
 
 
-def is_talent_data_valid(talent_data: dict) -> bool:
-
-    # Top-level structure is valid
-    if not are_talent_keys_valid(talent_data):
+def validate_talent_data(talent_data: dict) -> bool:
+    if TalentDataType.NAME not in talent_data:
+        return False
+    if set(talent_data) <= {TalentDataType.NAME, TalentDataType.ROOT}:
+        return False
+    if any(key not in TalentDataType for key in talent_data):
         return False
 
-    # Name is valid
-    if not is_name_valid(talent_data[NAME]):
+    if not validate_key(talent_data, TalentDataType.NAME, validate_name):
         return False
-
-    # Root is valid
-    if ROOT in talent_data and not is_name_valid(talent_data[ROOT]):
+    if not validate_key(talent_data, TalentDataType.ROOT, validate_name):
         return False
-
-    # Percent bonus is valid
-    if PERCENT_BONUS in talent_data and not is_percent_bonus_valid(talent_data[PERCENT_BONUS]):
+    if not validate_key(talent_data, TalentDataType.PERCENT_BONUS, validate_percent_bonus):
         return False
-
-    # Absolute bonus is valid
-    if ABSOLUTE_BONUS in talent_data and not is_absolute_bonus_valid(talent_data[ABSOLUTE_BONUS]):
+    if not validate_key(talent_data, TalentDataType.ABSOLUTE_BONUS, validate_absolute_bonus):
+        return False
+    if not validate_key(talent_data, TalentDataType.SPECIALIZATION, validate_specialization):
         return False
 
     return True
 
 
-def are_talent_keys_valid(talent_data: dict) -> bool:
-
-    # Name is provided
-    if NAME not in talent_data:
-        return False
-
-    # Data other than name and root is provided
-    if set(talent_data) <= {NAME, ROOT}:
-        return False
-
-    # Keys are talent data types
-    if any(key not in TALENT_DATA_TYPES for key in talent_data):
-        return False
-
-    return True
+def validate_key(talent_data: dict, key: str, validator_fn: Callable) -> bool:
+    return key not in talent_data or validator_fn(talent_data[key])
 
 
-def is_name_valid(name: Any) -> bool:
-
-    # Name is a string
+def validate_name(name: Any) -> bool:
     if not isinstance(name, str):
         return False
-
-    # Name isn't too short or too long
-    if not MIN_NAME_LEN <= len(name) <= MAX_NAME_LEN:
+    if not MIN_NAME_LENGTH <= len(name) <= MAX_NAME_LENGTH:
         return False
-
-    # Name has letters
     if not any(c in string.ascii_letters for c in name):
         return False
-
-    # Name uses only letters and spaces
     if any(c not in string.ascii_letters + " " for c in name):
         return False
-
     return True
 
 
-def is_percent_bonus_valid(percent_bonus: Any) -> bool:
-    valid = is_bonus_data_valid(percent_bonus, PercentBonus)
-    return valid
+def validate_percent_bonus(percent_bonus: Any) -> bool:
+    return validate_numeric_bonus(percent_bonus, PercentBonus)
 
 
-def is_absolute_bonus_valid(absolute_bonus: Any) -> bool:
-    valid = is_bonus_data_valid(absolute_bonus, AbsoluteBonus)
-    return valid
+def validate_absolute_bonus(absolute_bonus: Any) -> bool:
+    return validate_numeric_bonus(absolute_bonus, AbsoluteBonus)
 
 
-def is_bonus_data_valid(bonus_data: Any, bonus_enum: Enum) -> bool:
-
-    # bonus_data is a dictionary
-    if not isinstance(bonus_data, dict):
+def validate_numeric_bonus(bonus_data: Any, bonus_enum: Enum) -> bool:
+    if not isinstance(bonus_data, dict) or not bonus_data:
         return False
-
-    # bonus_data has items
-    if not bonus_data:
+    if any(k not in bonus_enum or not validate_bonus_lookup(v) for k, v in bonus_data.items()):
         return False
-
-    for key, lookup in bonus_data.items():
-
-        # keys are bonus_enum members
-        if key not in bonus_enum.__members__:
-            return False
-
-        # lookups are valid
-        if not is_bonus_lookup_valid(lookup):
-            return False
-
     return True
 
 
-def is_bonus_lookup_valid(lookup: Any) -> bool:
-
-    # lookup is a dictionary
-    if not isinstance(lookup, dict):
+def validate_bonus_lookup(lookup: Any) -> bool:
+    if not isinstance(lookup, dict) or not lookup:
         return False
 
-    # lookup has items
-    if not lookup:
-        return False
-
-    # lookup has at least one positive value
     any_pos_vals: bool = False
-
     for key, value in lookup.items():
-
-        # keys are talent ranks
         if key not in TALENT_RANKS:
             return False
-
-        # values are ints or floats
-        if not isinstance(value, (int, float)):
+        if not isinstance(value, (int, float)) or value < 0:
             return False
+        if value > 0:
+            any_pos_vals = True
 
-        # values are non-negative
-        if value < 0:
-            return False
+    return any_pos_vals
 
-        any_pos_vals |= value > 0
 
-    if not any_pos_vals:
+def validate_specialization(spec: Any) -> bool:
+    if not isinstance(spec, dict) or not spec:
         return False
-
+    if any(k not in TALENT_RANKS or v not in Specialization for k, v in spec.items()):
+        return False
     return True
-
-
-def is_specialization_valid(spec: Any) -> bool:
-    ...
 
 
 def _test() -> None:
     from pathlib import Path
     fn = str(Path(__file__).parent / "Vanguard.json")
-    valid = is_talent_file_valid(fn)
-    print(valid)
+    assert validate_talent_file(fn), f"{fn} is invalid!"
+    print(f"Test passed!")
 
 
 if __name__ == "__main__":
--- src/data/venums.py
+++ src/data/venums.py
@@ -1,11 +1,27 @@
-from enum import auto, Enum
+from enum import StrEnum
 
 
-class PercentBonus(Enum):
-    BIOTIC_PROTECTION = auto()
-    PISTOL_DAMAGE = auto()
-    SHOTGUN_DAMAGE = auto()
+class TalentDataType(StrEnum):
+    NAME = "name"
+    ROOT = "root"
+    BASE_VALUE = "base_value"
+    ABSOLUTE_BONUS = "absolute_bonus"
+    PERCENT_BONUS = "percent_bonus"
+    ABILITY = "ability"
+    SPECIALIZATION = "specialization"
 
 
-class AbsoluteBonus(Enum):
+class AbsoluteBonus(StrEnum):
     ...
+
+
+class PercentBonus(StrEnum):
+    BIOTIC_PROTECTION = "BIOTIC_PROTECTION"
+    PISTOL_DAMAGE = "PISTOL_DAMAGE"
+    SHOTGUN_DAMAGE = "SHOTGUN_DAMAGE"
+
+
+class Specialization(StrEnum):
+    ...
+
+
