314dfdd83cd3553959db49943700bad5323b6a3c
futurebutt <foretoldposterior@gmail.com>
2025-01-03 11:43:12 -0500

    bunch of refactoring for validation

--- src/data/validate.py
+++ src/data/validate.py
@@ -1,102 +1,172 @@
 import json
 import string
-from pathlib import Path
+from typing import Any
 
 from venums import *
 
 
 TALENT_RANKS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-TALENT_DATA_TYPES = ["root", "name", "percent_bonus", "absolute_bonus", "specialization"]
+TALENT_DATA_TYPES = [
+    NAME := "name",
+    ROOT := "root",
+    PERCENT_BONUS := "percent_bonus",
+    ABSOLUTE_BONUS := "absolute_bonus",
+    SPECIALIZATION := "specialization"
+]
+MIN_NAME_LEN = 1
+MAX_NAME_LEN = 16
 
 
-def is_name_valid(name: str) -> bool:
+def is_talent_file_valid(filename: str) -> bool:
+
+    with open(filename, "r") as f:
+        talent_data: dict = json.load(f)
+
+    valid = is_talent_data_valid(talent_data)
+    return valid
+
+
+def is_talent_data_valid(talent_data: dict) -> bool:
+
+    # Top-level structure is valid
+    if not are_talent_keys_valid(talent_data):
+        return False
+
+    # Name is valid
+    if not is_name_valid(talent_data[NAME]):
+        return False
+
+    # Root is valid
+    if ROOT in talent_data and not is_name_valid(talent_data[ROOT]):
+        return False
+
+    # Percent bonus is valid
+    if PERCENT_BONUS in talent_data and not is_percent_bonus_valid(talent_data[PERCENT_BONUS]):
+        return False
+
+    # Absolute bonus is valid
+    if ABSOLUTE_BONUS in talent_data and not is_absolute_bonus_valid(talent_data[ABSOLUTE_BONUS]):
+        return False
+
+    return True
+
+
+def are_talent_keys_valid(talent_data: dict) -> bool:
+
+    # Name is provided
+    if NAME not in talent_data:
+        return False
+
+    # Data other than name and root is provided
+    if set(talent_data) <= {NAME, ROOT}:
+        return False
+
+    # Keys are talent data types
+    if any(key not in TALENT_DATA_TYPES for key in talent_data):
+        return False
+
+    return True
+
+
+def is_name_valid(name: Any) -> bool:
 
     # Name is a string
     if not isinstance(name, str):
         return False
 
     # Name isn't too short or too long
-    if not 1 <= len(name) <= 16:
+    if not MIN_NAME_LEN <= len(name) <= MAX_NAME_LEN:
         return False
 
     # Name has letters
-    if not any([c in string.ascii_letters for c in name]):
+    if not any(c in string.ascii_letters for c in name):
         return False
 
-    # Name uses only letters or spaces
-    if any([c not in string.ascii_letters + " " for c in name]):
+    # Name uses only letters and spaces
+    if any(c not in string.ascii_letters + " " for c in name):
         return False
 
     return True
 
 
-def is_percent_bonus_valid(pb: dict) -> bool:
+def is_percent_bonus_valid(percent_bonus: Any) -> bool:
+    valid = is_bonus_data_valid(percent_bonus, PercentBonus)
+    return valid
 
-    # pb is a dictionary
-    if not isinstance(pb, dict):
+
+def is_absolute_bonus_valid(absolute_bonus: Any) -> bool:
+    valid = is_bonus_data_valid(absolute_bonus, AbsoluteBonus)
+    return valid
+
+
+def is_bonus_data_valid(bonus_data: Any, bonus_enum: Enum) -> bool:
+
+    # bonus_data is a dictionary
+    if not isinstance(bonus_data, dict):
         return False
 
-    # pb has items
-    if not pb:
+    # bonus_data has items
+    if not bonus_data:
         return False
 
-    for key, lookup in pb.items():
+    for key, lookup in bonus_data.items():
 
-        # key is a PercentBonus
-        if key not in PercentBonus.__members__:
+        # keys are bonus_enum members
+        if key not in bonus_enum.__members__:
             return False
 
-        # lookup is a dictionary
-        if not isinstance(lookup, dict):
-            return False
-
-        # lookup's keys are talent ranks:
-        if not all([k in TALENT_RANKS for k in lookup.keys()]):
-            return False
-
-        # lookup's values are ints or floats
-        if not all([isinstance(v, (int, float)) for v in lookup.values()]):
-            return False
-
-        # lookup's values are non-negative
-        if any([v < 0 for v in lookup.values()]):
-            return False
-
-        # at least one lookup value is positive
-        if not any([v > 0 for v in lookup.values()]):
+        # lookups are valid
+        if not is_bonus_lookup_valid(lookup):
             return False
 
     return True
 
 
-def is_talent_valid() -> bool:
+def is_bonus_lookup_valid(lookup: Any) -> bool:
 
-    with open(Path(__file__).parent / "Vanguard.json", "r") as f:
-        talent_data: dict = json.load(f)
-
-    # Name is provided
-    if "name" not in talent_data:
+    # lookup is a dictionary
+    if not isinstance(lookup, dict):
         return False
 
-    # Enough data is provided
-    if len(talent_data) <= 1:
+    # lookup has items
+    if not lookup:
         return False
 
-    # Data types are valid
-    if any([data not in TALENT_DATA_TYPES for data in talent_data]):
-        return False
+    # lookup has at least one positive value
+    any_pos_vals: bool = False
 
-    # Name is valid
-    if not is_name_valid(talent_data["name"]):
-        return False
+    for key, value in lookup.items():
 
-    # Percent-bonus data is valid
-    if not is_percent_bonus_valid(talent_data["percent_bonus"]):
+        # keys are talent ranks
+        if key not in TALENT_RANKS:
+            return False
+
+        # values are ints or floats
+        if not isinstance(value, (int, float)):
+            return False
+
+        # values are non-negative
+        if value < 0:
+            return False
+
+        any_pos_vals |= value > 0
+
+    if not any_pos_vals:
         return False
 
     return True
+
+
+def is_specialization_valid(spec: Any) -> bool:
+    ...
+
+
+def _test() -> None:
+    from pathlib import Path
+    fn = str(Path(__file__).parent / "Vanguard.json")
+    valid = is_talent_file_valid(fn)
+    print(valid)
 
 
 if __name__ == "__main__":
-    valid = is_talent_valid()
-    print(valid)
+    _test()
--- src/data/venums.py
+++ src/data/venums.py
@@ -5,3 +5,7 @@ class PercentBonus(Enum):
     BIOTIC_PROTECTION = auto()
     PISTOL_DAMAGE = auto()
     SHOTGUN_DAMAGE = auto()
+
+
+class AbsoluteBonus(Enum):
+    ...
